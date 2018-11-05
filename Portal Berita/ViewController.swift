15d30faea68618250dcb32b1fe0bfb63955655b9
Yoenas <yoenas@yoenas.local>
2018-11-05 15:00:05 +0700

    Initial Commit

---
+++ Portal Berita/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Portal Berita
+//
+//  Created by Yoenas on 05/11/18.
+//  Copyright © 2018 Yoenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
